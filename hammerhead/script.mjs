e38228887c7ef552bbab871374e26bcbbe3631f9
Subzero <subzeroi386@outlook.com>
2024-11-17 18:43:15 -0500

    Update script.mjs

--- hammerhead/script.mjs
+++ hammerhead/script.mjs
@@ -1,9 +1,5 @@
-import express from "express";
-import { createServer } from "node:http";
 import { createRammerhead, shouldRouteRh, routeRhUpgrade, routeRhRequest } from '@rubynetwork/rammerhead';
 
-const app = express();
-const server = createServer();
 const rh = createRammerhead({
     logLevel: 'debug', //Options are: disabled, debug, traffic, info, warn, error (default: debug)
     reverseProxy: false, //whether or not this server is running behind a reverse proxy (default: false)
@@ -11,27 +7,17 @@ const rh = createRammerhead({
     disableHttp2: false //disable http2 usage (default: false) (NOT RECOMMENDED)
 })
 
-server.on("request", (req, res) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  if (shouldRouteRh(req)) {
-    routeRhRequest(rh /* from the createRammerhead function MUST be passed */, req, res)
-  }
-  else {
-    app(req, res);
-  }
-});
-server.on("upgrade", (req, socket, head) => {    
-    if (rammerhead.shouldRouteRh(req)) {
-        rammerhead.routeRhUpgrade(rh /* from the createRammerhead funtion MUST be passed */, req, socket, head)
+const server = http.createServer();
+server.on('request', (req, res) => {
+    if (shouldRouteRh(req)) {
+        routeRhRequest(rh /* from the createRammerhead function MUST be passed */, req, res)
     }
-    else {
-        socket.end()
+})
+server.on('upgrade', (req, socket, head) => {
+    if (shouldRouteRh(req)) {
+        routeRhUpgrade(rh /* from the createRammerhead funtion MUST be passed */, req, socket, head)
     }
-});
-
-server.on("listening", () => {
-  console.log("Listening on: http://localhost:8080");
-});
-
-server.listen({port: 8080});
+})
+server.listen({host: '0.0.0.0', port: 8080}, () => {
+    console.log('Server is listening on port 8080!');
+})
